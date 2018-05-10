2b89cc71b150abe0883785504284c5f84090093c
William Wold <william.wold@canonical.com>
2018-05-10 14:27:45 -0700

    updated copyright dates

--- tests/test_xdg_surface_v6.cpp
+++ tests/test_xdg_surface_v6.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2012 Intel Corporation
  * Copyright © 2013 Collabora, Ltd.
- * Copyright © 2017 Canonical Ltd.
+ * Copyright © 2018 Canonical Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
