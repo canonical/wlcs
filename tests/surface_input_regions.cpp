cd36c62f6381352cb5dcf3b1060ef37ac7f0537e
William Wold <william.wold@canonical.com>
2018-05-24 18:42:14 -0700

    added input region touch test

--- tests/surface_input_regions.cpp
+++ tests/surface_input_regions.cpp
@@ -127,6 +127,82 @@ TEST_P(InputRegionPointerEnterTest, pointer_enter_and_leave_input_region)
     EXPECT_THAT(client.focused_window(), Ne(wl_surface));
 }
 
+TEST_P(InputRegionPointerEnterTest, touch_enter_leave)
+{
+    using namespace testing;
+
+    wlcs::Client client{the_server()};
+
+    auto const params = GetParam();
+
+    auto surface = client.create_visible_surface(
+        params.window_width,
+        params.window_height);
+
+    int const top_left_x = 64, top_left_y = 7;
+    the_server().move_surface_to(surface, top_left_x, top_left_y);
+
+    auto const wl_surface = static_cast<struct wl_surface*>(surface);
+
+    auto const wl_region = wl_compositor_create_region(client.compositor());
+    for (auto const& e: params.region.elements)
+    {
+        switch(e.action)
+        {
+        case RegionAction::add:
+            wl_region_add(wl_region, e.x, e.y, e.width, e.height);
+            break;
+        case RegionAction::subtract:
+            wl_region_subtract(wl_region, e.x, e.y, e.width, e.height);
+            break;
+        }
+    }
+    wl_surface_set_input_region(wl_surface, wl_region);
+    wl_region_destroy(wl_region);
+    wl_surface_commit(wl_surface);
+    client.roundtrip();
+
+    auto touch = the_server().create_touch();
+    int touch_x = top_left_x + params.initial_x;
+    int touch_y = top_left_y + params.initial_y;
+    touch.down_at(touch_x, touch_y);
+
+    client.roundtrip();
+    usleep(100000);
+    client.roundtrip();
+
+    EXPECT_THAT(client.touched_window(), Ne(wl_surface));
+
+    /* move touch; it should now be inside the surface */
+    touch.up();
+    touch.down_at(touch_x + params.dx, touch_y + params.dy);
+
+    client.roundtrip();
+    usleep(100000);
+    client.roundtrip();
+
+    EXPECT_THAT(client.touched_window(), Eq(wl_surface));
+    EXPECT_THAT(client.touch_position(),
+                Eq(std::make_pair(
+                    wl_fixed_from_int(params.initial_x + params.dx),
+                    wl_fixed_from_int(params.initial_y + params.dy))));
+
+    /* move touch back; it should now be outside the surface */
+    touch.up();
+    touch.down_at(touch_x, touch_y);
+
+    client.roundtrip();
+    usleep(100000);
+    client.roundtrip();
+
+    EXPECT_THAT(client.touched_window(), Ne(wl_surface));
+
+    touch.up();
+    client.roundtrip();
+    usleep(100000);
+    client.roundtrip();
+}
+
 InputRegion const full_surface_region{"full surface", {
     {RegionAction::add, 0, 0, RegionAndMotion::window_width, RegionAndMotion::window_height}}};
 
