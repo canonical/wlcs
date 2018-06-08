6c6a02e4d573b1bcba46085f76c1d30dcbe7f91e
Alan Griffiths <alan@octopull.co.uk>
2018-06-08 12:16:36 +0100

    A bunch more tests written whilst debugging the test framework

--- tests/self_test.cpp
+++ tests/self_test.cpp
@@ -40,6 +40,9 @@ struct SelfTest : StartedInProcessServer
 {
     Client client1{the_server()};
 };
+
+auto static const any_width = 100;
+auto static const any_height = 100;
 }
 
 TEST_F(SelfTest, when_creating_second_client_nothing_bad_happens)
@@ -51,4 +54,62 @@ TEST_F(SelfTest, given_second_client_when_roundtripping_first_client_nothing_bad
 {
     Client client2{the_server()};
     client1.roundtrip();
+}
+
+TEST_F(SelfTest, given_second_client_when_roundtripping_both_clients_nothing_bad_happens)
+{
+    Client client2{the_server()};
+
+    for (auto i = 0; i != 10; ++i)
+    {
+        client1.roundtrip();
+        client2.roundtrip();
+    }
+}
+
+TEST_F(SelfTest, when_a_client_creates_a_surface_nothing_bad_happens)
+{
+    Surface const surface1{client1.create_visible_surface(any_width, any_height)};
+    std::shared_ptr<ShmBuffer> const buffer1{std::make_shared<ShmBuffer>(client1, any_width, any_height)};
+    wl_surface_attach(surface1, *buffer1, 0, 0);
+    wl_surface_commit(surface1);
+
+    client1.roundtrip();
+}
+
+TEST_F(SelfTest, given_second_client_when_first_creates_a_surface_nothing_bad_happens)
+{
+    Client client2{the_server()};
+
+    Surface const surface1{client1.create_visible_surface(any_width, any_height)};
+    std::shared_ptr<ShmBuffer> const buffer1{std::make_shared<ShmBuffer>(client1, any_width, any_height)};
+    wl_surface_attach(surface1, *buffer1, 0, 0);
+    wl_surface_commit(surface1);
+
+    for (auto i = 0; i != 10; ++i)
+    {
+        client1.roundtrip();
+        client2.roundtrip();
+    }
+}
+
+TEST_F(SelfTest, given_second_client_when_both_create_a_surface_nothing_bad_happens)
+{
+    Client client2{the_server()};
+
+    Surface const surface1{client1.create_visible_surface(any_width, any_height)};
+    std::shared_ptr<ShmBuffer> const buffer1{std::make_shared<ShmBuffer>(client1, any_width, any_height)};
+    wl_surface_attach(surface1, *buffer1, 0, 0);
+    wl_surface_commit(surface1);
+
+    Surface const surface2{client2.create_visible_surface(any_width, any_height)};
+    std::shared_ptr<ShmBuffer> const buffer2{std::make_shared<ShmBuffer>(client2, any_width, any_height)};
+    wl_surface_attach(surface2, *buffer2, 0, 0);
+    wl_surface_commit(surface2);
+
+    for (auto i = 0; i != 10; ++i)
+    {
+        client1.roundtrip();
+        client2.roundtrip();
+    }
 }
