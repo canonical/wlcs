65b0ba9be2167c3ed96292a9b490fa24f863ff20
William Wold <william.wold@canonical.com>
2018-08-08 08:48:23 -0700

    maybe this?

--- tests/xdg_popup_v6.cpp
+++ tests/xdg_popup_v6.cpp
@@ -181,7 +181,7 @@ TEST_P(XdgPopupV6Test, positioner_places_popup_correctly)
 
     map_popup();
 
-    ASSERT_TRUE(state) << "popup configure event not sent";
+    ASSERT_TRUE(static_cast<bool>(state)) << "popup configure event not sent";
     ASSERT_THAT(std::make_pair(state.value().x, state.value().y), Eq(param.expected_positon)) << "popup placed in incorrect position";
 }
 
