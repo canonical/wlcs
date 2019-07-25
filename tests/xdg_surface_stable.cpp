fd3acfc5cf73b824b5571a1083a04f7d33cf95de
William Wold <wm@wmww.sh>
2019-07-25 13:16:58 +0000

    Add test for https://github.com/MirServer/mir/issues/936

--- tests/xdg_surface_stable.cpp
+++ tests/xdg_surface_stable.cpp
@@ -179,3 +179,29 @@ TEST_F(XdgSurfaceStableTest, attaching_buffer_to_unconfigured_xdg_surface_is_an_
 
     FAIL() << "Expected protocol error not received";
 }
+
+// Regression test for https://github.com/MirServer/mir/issues/936
+TEST_F(XdgSurfaceStableTest, is_activated_when_buffer_sent_on_first_commit)
+{
+    wlcs::Client client{the_server()};
+    wlcs::Surface surface{client};
+    wlcs::XdgSurfaceStable xdg_surface{client, surface};
+    wlcs::XdgToplevelStable toplevel{xdg_surface};
+
+    wlcs::XdgToplevelStable::State state{0, 0, nullptr};
+    xdg_surface.add_configure_notification([&](uint32_t serial)
+        {
+            xdg_surface_ack_configure(xdg_surface, serial);
+        });
+    toplevel.add_configure_notification([&](int32_t width, int32_t height, struct wl_array *states)
+        {
+            state = wlcs::XdgToplevelStable::State{width, height, states};
+        });
+
+    auto surface_rendered = std::make_shared<bool>(false);
+    surface.add_frame_callback([surface_rendered](auto) { *surface_rendered = true; });
+    wl_surface_commit(surface);
+    client.dispatch_until([surface_rendered]() { return *surface_rendered; });
+
+    EXPECT_THAT(state.activated, Eq(true));
+}
