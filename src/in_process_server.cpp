25dca26871d62442bc585a53464ccb8c0c1f08dd
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2019-08-07 15:11:24 +1000

    use std::chrono for better looking code

--- src/in_process_server.cpp
+++ src/in_process_server.cpp
@@ -920,7 +920,9 @@ public:
     void dispatch_until(
         std::function<bool()> const& predicate, std::chrono::seconds timeout)
     {
-        auto const end_time = std::chrono::steady_clock::now() + timeout;
+        using namespace std::chrono;
+
+        auto const end_time = steady_clock::now() + timeout;
         while (!predicate())
         {
             while (wl_display_prepare_read(display) != 0)
@@ -930,7 +932,7 @@ public:
             }
             wl_display_flush(display);
 
-            auto const time_left = end_time - std::chrono::steady_clock::now();
+            auto const time_left = end_time - steady_clock::now();
             if (time_left.count() < 0)
             {
                 BOOST_THROW_EXCEPTION((Timeout{"Timeout waiting for condition"}));
@@ -945,9 +947,7 @@ public:
              *
              * Adding 1ms will ensure we wait until *after* we're meant to timeout.
              */
-            auto const maximum_wait_ms =
-                std::chrono::duration_cast<std::chrono::milliseconds>(time_left) +
-                    std::chrono::milliseconds{1};
+            auto const maximum_wait_ms = duration_cast<milliseconds>(time_left) + 1ms;
             pollfd fd{
                 wl_display_get_fd(display),
                 POLLIN | POLLERR,
