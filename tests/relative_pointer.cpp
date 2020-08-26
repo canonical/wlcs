623376a675d1d2f7207cde1be2e7c395889f7cfb
Alan Griffiths <alan@octopull.co.uk>
2020-08-26 16:21:44 +0100

    Relative pointer gets movement

--- tests/relative_pointer.cpp
+++ tests/relative_pointer.cpp
@@ -27,13 +27,29 @@ using namespace wlcs;
 
 namespace
 {
-auto static const any_width = 100;
-auto static const any_height = 100;
+auto const any_width = 300;
+auto const any_height = 300;
+auto const middle_x = any_width / 2;
+auto const middle_y = any_height / 2;
 
 struct RelativePointer : StartedInProcessServer
 {
+    // Create a client with a surface and a cursor centered on the surface
     Client a_client{the_server()};
-    Surface const a_surface{a_client.create_visible_surface(any_width, any_height)};
+    Surface a_surface{a_client.create_visible_surface(any_width, any_height)};
+    Pointer cursor = the_server().create_pointer();
+
+    ZwpRelativePointerManagerV1 manager{a_client};
+    ZwpRelativePointerV1 pointer{manager, a_client.the_pointer()};
+
+    void SetUp() override
+    {
+        StartedInProcessServer::SetUp();
+
+        // Get the surface in a known position with the cursor over it
+        the_server().move_surface_to(a_surface, 0, 0);
+        cursor.move_to(middle_x, middle_y);
+    }
 
     void TearDown() override
     {
@@ -45,8 +61,17 @@ struct RelativePointer : StartedInProcessServer
 
 TEST_F(RelativePointer, can_get_relative_pointer)
 {
-    ZwpRelativePointerManagerV1 manager{a_client};
-    ZwpRelativePointerV1 pointer{manager, a_client.the_pointer()};
-
     EXPECT_THAT(pointer, NotNull());
+}
+
+TEST_F(RelativePointer, relative_pointer_gets_movement)
+{
+    auto const move_x = any_width/6;
+    auto const move_y = any_height/6;
+
+    EXPECT_CALL(pointer, relative_motion(_, _,
+                                         wl_fixed_from_int(move_x), wl_fixed_from_int(move_y),
+                                         wl_fixed_from_int(move_x), wl_fixed_from_int(move_y))).Times(1);
+
+    cursor.move_by(move_x, move_y);
 }
